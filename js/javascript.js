aabfeefd750075cd73bc56fc0c9f036405fd5beb
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-28 00:46:31 -0300

    feat: transformando valor inserido em input em moeda local

--- js/javascript.js
+++ js/javascript.js
@@ -13,9 +13,12 @@ custom.style.fontSize = '15px'
 custom.style.textAlign = 'center'
 
 
+// recebe o valor da conta
 function operationValor (){
-  var valor = inputValor.value    
+  var valor = inputValor.value 
+  inputValor.value = Number(valor).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) 
   Number(valor)
+  console.log(valor)
   
   if(valor > 0){
     msgValor.innerText = "Tudo certo!"
@@ -28,7 +31,29 @@ function operationValor (){
   }
 }
 
+// recebe o percentual da gorjeta 
+buttons.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    const gorjeta = e.target.value
+    console.log(gorjeta)
+  })
+})
 
+
+function customOperation (){  
+  gorjeta = custom.value/100
+  if(gorjeta > 0){
+  Number(gorjeta)
+  console.log(gorjeta)
+  }else {
+    custom.placeholder = 'inválido'    
+    custom.value = ""
+    custom.focus()
+    
+  }
+}
+
+// recebe o valor da quantida de pessoas
 function operationPeople (){
     var people = inputPeople.value
     Number(people)
@@ -47,26 +72,7 @@ function operationPeople (){
 
 
 
-buttons.forEach((btn)=>{
-  btn.addEventListener('click',(e)=>{
-    const gorjeta = e.target.value
-    console.log(gorjeta)
-  })
-})
 
-
-function customOperation (){  
-  gorjeta = custom.value/100
-  if(gorjeta > 0){
-  Number(gorjeta)
-  console.log(gorjeta)
-  }else {
-    window.alert('Digite um número')
-    custom.value = ""
-    custom.focus()
-    
-  }
-}
 
 
 
